e0272a98d9bef152a097e537d6e017e9b83afc28
ysur67 <rus.muratow@gmail.com>
2021-11-24 14:52:12 +0300

    write image data to bytes array

--- main.go
+++ main.go
@@ -7,22 +7,38 @@ import (
 	"github.com/gotk3/gotk3/gtk"
 )
 
-type Cell struct {
-	points int
+func writeCircle(buffer *[]uint, width, height int, foreground, background uint, radius int) {
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			dx := width - x*2 - 1
+			dy := height - y*2 - 1
+			r := radius * radius
+			if (dx*dx)+(dy*dy) <= (r) {
+				(*buffer)[y*width+x] = foreground
+			} else {
+				(*buffer)[y*width+x] = background
+			}
+		}
+	}
 }
 
 var BYTES_PER_PIXEL int = 3
 
-func toBytes(cells *[]Cell, width, height int) []byte {
-	out := make([]byte, len(*cells)*BYTES_PER_PIXEL)
-	for i := 0; i < len(out); i += 3 {
+func toBytes(cells *[]uint, width, height int) []byte {
+	var out []byte
+	for i := 0; i < len((*cells)); i++ {
+		pixel := (*cells)[i]
 		for j := 0; j < BYTES_PER_PIXEL; j++ {
-			index := i + j
+			shiftValue := 0
+			// TODO: ref this
 			if j == 0 {
-				out[index] = 255
+				shiftValue = 16
+			} else if j == 1 {
+				shiftValue = 8
 			} else {
-				out[index] = 0
+				shiftValue = 0
 			}
+			out = append(out, byte(pixel>>uint(shiftValue)&0xff))
 		}
 	}
 	return out
@@ -41,7 +57,10 @@ func main() {
 	})
 	WIDTH := 800
 	HEIGHT := 600
-	cells := make([]Cell, WIDTH*HEIGHT)
+	var FOREGROUND uint = 0xff0000
+	var BACKGROUND uint = 0x0000ff
+	cells := make([]uint, WIDTH*HEIGHT)
+	writeCircle(&cells, WIDTH, HEIGHT, FOREGROUND, BACKGROUND, HEIGHT)
 	buff, err := gdk.PixbufNewFromData(toBytes(&cells, WIDTH, HEIGHT), gdk.COLORSPACE_RGB, false, 8, WIDTH, HEIGHT, BYTES_PER_PIXEL*WIDTH)
 	if err != nil {
 		panic(err)
