79fd32a7075d3c5cab9c3650d31c366fe740fabf
ysur67 <rus.muratow@gmail.com>
2021-11-24 11:53:20 +0300

    add gtk3 dependency

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gotk3/gotk3/gdk"
+	"github.com/gotk3/gotk3/gtk"
+)
+
 func main() {
-	println("hjerllo")
+	gtk.Init(nil)
+
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
+	}
+	win.SetTitle("Sandpile")
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+	WIDTH := 800
+	HEIGHT := 600
+	pixelBuffer, err := gdk.PixbufNew(gdk.COLORSPACE_RGB, false, 8, WIDTH, HEIGHT)
+	if err != nil {
+		panic(err)
+	}
+	image, err := gtk.ImageNewFromPixbuf(pixelBuffer)
+	if err != nil {
+		panic(err)
+	}
+	win.Add(image)
+	win.SetDefaultSize(800, 600)
+	win.ShowAll()
+	gtk.Main()
 }
